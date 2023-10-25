d5c53e7d95bfc9b433425edf262989701596d9e0
hana <rlagksk8428@gmail.com>
2023-10-26 08:07:51 +0900

    feat: add dicom de-id script, add status label

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import sys
 from PyQt5 import QtWidgets, uic
+from PyQt5.QtCore import QThread, pyqtSignal
 
 from MainWindow import Ui_MainWindow
+from dicom_deidentifier import main
+
+class WorkerThread(QThread):
+    finished = pyqtSignal()
+
+    def __init__(self, dicom_folder):
+        super().__init__()
+        self.dicom_folder = dicom_folder
+
+    def run(self):
+        main(self.dicom_folder)
+        self.finished.emit()
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -9,7 +22,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
 
-        self.pushButton.clicked.connect(self.getDirectory)
+        self.selectButton.clicked.connect(self.getDirectory)
         self.submitButton.clicked.connect(self.submitBtnclick)
 
         # 인스턴스 변수로 dicom_folder를 선언
@@ -19,15 +32,23 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         folder = QtWidgets.QFileDialog.getExistingDirectory(self, "Select DICOM Directory")
         if folder:  # 사용자가 디렉토리를 선택했다면
             self.dicom_folder = folder
+            self.folderPath.setText(self.dicom_folder)
             print("선택된 DICOM 디렉토리:", self.dicom_folder)
 
     def submitBtnclick(self):
         if self.dicom_folder:
-            print("submit dicom path")
-            
+            self.statusLabel.setText(" 🔄 Processing...")
+
+            self.worker_thread = WorkerThread(self.dicom_folder)
+            self.worker_thread.finished.connect(self.on_main_finished)
+            self.worker_thread.start()
         else:
             print("DICOM 디렉토리가 선택되지 않았습니다.")
+            self.statusLabel.setText(" 🔵 DICOM 디렉토리가 선택되지 않았습니다.")
 
+    def on_main_finished(self):
+        print("submit dicom path")
+        self.statusLabel.setText(" ✅ complete DICOM de-Identifier")
 
 
 app = QtWidgets.QApplication(sys.argv)
